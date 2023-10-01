f57479e627876edd8aa01e02ddb9d2b3700ef207
Lokesh Jain <luckycreationsindia@gmail.com>
2023-10-01 14:53:31 +0530

    *Add reset function

--- lib/src/ui/home.dart
+++ lib/src/ui/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:retail_app/main.dart';
+import 'package:retail_app/src/utils/isar_service.dart';
+import 'package:retail_app/src/utils/utils.dart';
+import 'package:retail_app/src/utils/globals.dart' as globals;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -26,8 +30,30 @@ class _HomePageState extends State<HomePage> {
             child: const Text("Orders"),
             onPressed: () => context.push('/orders'),
           ),
+          TextButton(
+            child: const Text("Reset"),
+            onPressed: () {
+              showLoading(context, title: "Clearing Data");
+              clearData().then((value) {
+                initMainData();
+              }).catchError((err) {
+                printDebug(err);
+              }).whenComplete(() {
+                IsarService().openDB();
+                showSnackMessage(context, "Data Cleared!");
+                Navigator.pop(context);
+              });
+            },
+          ),
         ],
       ),
     );
+  }
+
+  Future<void> clearData() async {
+    final isar = globals.db;
+    await isar.writeAsync((isar) {
+      isar.clear();
+    });
   }
 }
